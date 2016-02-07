c9e87ea6e7d2c9ee1a9856a7a2f1f1e1fe915a4d
Mac <mac@abctech-thailand.com>
2016-02-07 16:05:42 +0700

    Done codehew 2016, final 1

---
+++ src/main/java/com/mackittipat/codekata/codehew2016/final1/Final1Cluster.java
@@ -0,0 +1,94 @@
+package com.mackittipat.codekata.codehew2016.final1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Final1Cluster {
+
+    public int clusterSize(List<Coordinate> inputCoordinateList) {
+
+        int size = 0;
+        List<Coordinate> resultCoordinateList;
+
+        for(Coordinate inputCoordinate : inputCoordinateList) {
+            System.out.println("Calculating neighbor size for " + inputCoordinate.toString());
+            resultCoordinateList = new ArrayList<>();
+            clusterSize(inputCoordinate, inputCoordinateList, resultCoordinateList);
+            for(Coordinate resultCoordinate : resultCoordinateList) {
+                System.out.println(resultCoordinate);
+            }
+            if(resultCoordinateList.size() > size) {
+                size = resultCoordinateList.size();
+            }
+            System.out.println("==================================================");
+        }
+
+        return size;
+    }
+
+    private void clusterSize(Coordinate coordinate,
+                             List<Coordinate> coordinateList,
+                             List<Coordinate> resultCoordinateList) {
+
+        boolean stop = false;
+
+        while (!stop) {
+            List<Coordinate> neighborCoordinateList = findNeighbor(coordinate, coordinateList);
+            boolean allNeighborInResultList = true;
+            for(Coordinate neighborCoordinate : neighborCoordinateList) {
+                if(!isCoordinateInList(neighborCoordinate, resultCoordinateList)) {
+                    resultCoordinateList.add(neighborCoordinate);
+                    allNeighborInResultList = false;
+                    clusterSize(neighborCoordinate, coordinateList, resultCoordinateList);
+                    break;
+                }
+            }
+            if(allNeighborInResultList) {
+                stop = true;
+            }
+        }
+    }
+
+    private List<Coordinate> findNeighbor(Coordinate coordinate,
+                                         List<Coordinate> coordinateList) {
+
+//        System.out.println("Finding neighbor for " + coordinate.toString());
+
+        List<Coordinate> neighborCoordinateList = new ArrayList<>();
+
+        Coordinate coordinateNorth = new Coordinate(coordinate.getX(), coordinate.getY() - 1);
+        if(isCoordinateInList(coordinateNorth, coordinateList)) {
+            neighborCoordinateList.add(coordinateNorth);
+//            System.out.println("North neighbor = " + coordinateNorth.toString());
+        }
+        Coordinate coordinateEast = new Coordinate(coordinate.getX() + 1, coordinate.getY());
+        if(isCoordinateInList(coordinateEast, coordinateList)) {
+            neighborCoordinateList.add(coordinateEast);
+//            System.out.println("East neighbor = " + coordinateEast.toString());
+        }
+        Coordinate coordinateSouth = new Coordinate(coordinate.getX(), coordinate.getY() + 1);
+        if(isCoordinateInList(coordinateSouth, coordinateList)) {
+            neighborCoordinateList.add(coordinateSouth);
+//            System.out.println("South neighbor = " + coordinateSouth.toString());
+        }
+        Coordinate coordinateWest = new Coordinate(coordinate.getX() - 1, coordinate.getY());
+        if(isCoordinateInList(coordinateWest, coordinateList)) {
+            neighborCoordinateList.add(coordinateWest);
+//            System.out.println("West neighbor = " + coordinateWest.toString());
+        }
+
+        return neighborCoordinateList;
+    }
+
+    private boolean isCoordinateInList(Coordinate coordinate,
+                                       List<Coordinate> coordinateList) {
+
+        for(Coordinate coOrd : coordinateList) {
+            if(coordinate.getX() == coOrd.getX() && coordinate.getY() == coOrd.getY()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/com/mackittipat/codekata/codehew2016/final1/Final1ClusterTest.java
@@ -0,0 +1,123 @@
+package com.mackittipat.codekata.codehew2016.final1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Final1ClusterTest {
+    @Test
+    public void testClusterSize() {
+        Final1Cluster final1Cluster = new Final1Cluster();
+
+        List<Coordinate> coordinateList = new ArrayList<>();
+        coordinateList.add(new Coordinate(1, 0));
+        coordinateList.add(new Coordinate(2, 0));
+        coordinateList.add(new Coordinate(0, 1));
+        coordinateList.add(new Coordinate(1, 1));
+        coordinateList.add(new Coordinate(2, 1));
+        coordinateList.add(new Coordinate(4, 1));
+        coordinateList.add(new Coordinate(4, 2));
+
+        int result = final1Cluster.clusterSize(coordinateList);
+
+        Assert.assertEquals(5, result);
+    }
+
+    @Test
+    public void testClusterSize2() {
+        Final1Cluster final1Cluster = new Final1Cluster();
+
+        List<Coordinate> coordinateList = new ArrayList<>();
+        coordinateList.add(new Coordinate(2, 0));
+        coordinateList.add(new Coordinate(0, 1));
+        coordinateList.add(new Coordinate(1, 1));
+        coordinateList.add(new Coordinate(2, 1));
+        coordinateList.add(new Coordinate(4, 1));
+        coordinateList.add(new Coordinate(4, 2));
+
+        int result = final1Cluster.clusterSize(coordinateList);
+
+        Assert.assertEquals(4, result);
+    }
+
+    @Test
+    public void testClusterSize3() {
+        Final1Cluster final1Cluster = new Final1Cluster();
+
+        List<Coordinate> coordinateList = new ArrayList<>();
+        coordinateList.add(new Coordinate(2, 1));
+        coordinateList.add(new Coordinate(4, 1));
+        coordinateList.add(new Coordinate(4, 2));
+
+        int result = final1Cluster.clusterSize(coordinateList);
+
+        Assert.assertEquals(2, result);
+    }
+
+    @Test
+    public void testClusterSize4() {
+        Final1Cluster final1Cluster = new Final1Cluster();
+
+        List<Coordinate> coordinateList = new ArrayList<>();
+        coordinateList.add(new Coordinate(0, 3));
+        coordinateList.add(new Coordinate(1, 1));
+        coordinateList.add(new Coordinate(1, 2));
+        coordinateList.add(new Coordinate(1, 4));
+        coordinateList.add(new Coordinate(1, 5));
+        coordinateList.add(new Coordinate(1, 6));
+        coordinateList.add(new Coordinate(1, 8));
+        coordinateList.add(new Coordinate(2, 1));
+
+        int result = final1Cluster.clusterSize(coordinateList);
+
+        Assert.assertEquals(3, result);
+    }
+
+    @Test
+    public void testClusterSize5() {
+        Final1Cluster final1Cluster = new Final1Cluster();
+
+        List<Coordinate> coordinateList = new ArrayList<>();
+        coordinateList.add(new Coordinate(0, 3));
+        coordinateList.add(new Coordinate(1, 1));
+        coordinateList.add(new Coordinate(1, 2));
+        coordinateList.add(new Coordinate(1, 3));
+        coordinateList.add(new Coordinate(1, 4));
+        coordinateList.add(new Coordinate(1, 5));
+        coordinateList.add(new Coordinate(1, 6));
+        coordinateList.add(new Coordinate(1, 8));
+        coordinateList.add(new Coordinate(2, 1));
+
+        int result = final1Cluster.clusterSize(coordinateList);
+
+        Assert.assertEquals(8, result);
+    }
+
+    @Test
+    public void testClusterSize6() {
+        Final1Cluster final1Cluster = new Final1Cluster();
+
+        List<Coordinate> coordinateList = new ArrayList<>();
+        coordinateList.add(new Coordinate(0,0));
+        coordinateList.add(new Coordinate(0,1));
+        coordinateList.add(new Coordinate(0,3));
+        coordinateList.add(new Coordinate(0,4));
+        coordinateList.add(new Coordinate(0,5));
+        coordinateList.add(new Coordinate(0,6));
+        coordinateList.add(new Coordinate(1,2));
+        coordinateList.add(new Coordinate(1,5));
+        coordinateList.add(new Coordinate(2,0));
+        coordinateList.add(new Coordinate(2,1));
+        coordinateList.add(new Coordinate(2,4));
+        coordinateList.add(new Coordinate(2,5));
+        coordinateList.add(new Coordinate(3,0));
+        coordinateList.add(new Coordinate(3,3));
+        coordinateList.add(new Coordinate(3,5));
+
+        int result = final1Cluster.clusterSize(coordinateList);
+
+        Assert.assertEquals(8, result);
+    }
+}
